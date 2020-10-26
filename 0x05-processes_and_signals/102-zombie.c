165cab104458db794b57d38c1a80e6fbebe67211
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-10-26 21:43:42 +0100

    c program create process with fork

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - creates an infinite loop to make the program hang
+ *
+ * Return: always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	}
+
+/**
+ * main - creates zombie processes 5 times.
+ *
+ * Return: Nothing
+ */
+int main(void)
+{
+	int index;
+	pid_t zombie;
+
+	for (index = 0; index < 5; index++)
+	{
+	zombie = fork();
+	if (zombie == 0)
+		return (0);
+	printf("Zombie process created, PID: %d\n", zombie);
+	}
+
+	infinite_while();
+	return (0);
+}
