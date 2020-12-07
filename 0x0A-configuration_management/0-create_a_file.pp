3be331d93aafdc5cafbf9f770e1e55605c7a89a4
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-07 16:54:20 +0100

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#create a file "holberton" in /tmp
+
+file { 'holberton':
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+  }
