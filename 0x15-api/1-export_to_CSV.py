de5aaad8d844e98f5f582fa81e4e5a6a67eada5a
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-01 18:08:59 +0100

    please work :'(

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,15 +10,27 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    filename = argv[1] + ".csv"
+    completed_tasks = 0
+    total_tasks = 0
+    title = []
     id = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
     req = requests.get('https://jsonplaceholder.typicode.com/todos/')
     name = (id.json().get('name'))
-    with open(filename, "w") as f:
-        r_json = req.json()
-        for i in r_json:
-            if i.get('userId') == int(argv[1]):
-                w = csv.writer(f, quoting=csv.QUOTE_ALL)
-                w.writerow([i.get('userId'), name,
-                           i.get('completed'), i.get('title')])
+    r_json = req.json()
+    for i in r_json:
+        if i.get('userId') == int(argv[1]):
+            if i.get('completed') is True:
+                title.append(i.get('title'))
+                completed_tasks += 1
+            total_tasks += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, completed_tasks, total_tasks))
+    for i in title:
+        print("\t {}".format(i))
+
+    with open("{}.csv".format(argv[1]), "w") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in req.json():
+            writer.writerow([argv[1], name,
+                            task.get("completed"), task.get("title")])
