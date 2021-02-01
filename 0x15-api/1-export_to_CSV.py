664a934cd56f5014396e9b4ff036792d80a92f85
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-01 18:27:00 +0100

    please work :'(

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,7 +4,7 @@ returns information about his/her todo list progress."""
 
 import csv
 import requests
-from sys import *
+import sys
 
 
 if __name__ == '__main__':
@@ -13,12 +13,13 @@ if __name__ == '__main__':
     total_tasks = 0
     title = []
     id = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
     req = requests.get('https://jsonplaceholder.typicode.com/todos/')
     name = (id.json().get('name'))
+    username = (id.get("username"))
     r_json = req.json()
     for i in r_json:
-        if i.get('userId') == int(argv[1]):
+        if i.get('userId') == int(sys.argv[1]):
             if i.get('completed') is True:
                 title.append(i.get('title'))
                 completed_tasks += 1
@@ -28,8 +29,8 @@ if __name__ == '__main__':
     for i in title:
         print("\t {}".format(i))
 
-    with open("{}.csv".format(argv[1]), "w") as file:
+    with open("{}.csv".format(sys.argv[1]), "w") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in req.json():
-            writer.writerow([argv[1], name,
+            writer.writerow([sys.argv[1], username,
                             task.get("completed"), task.get("title")])
