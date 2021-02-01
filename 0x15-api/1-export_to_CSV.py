f62659d92a8f44c557f762e4f2d990f78ec10318
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-01 15:57:01 +0100

    push and check task1 to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" a Python script that, using this REST API, for a given employee ID,
+ returns information about his/her TODO list progress.
+"""
+
+
+import requests
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+
+    filename = argv[1] + ".csv"
+    id = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    req = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    name = (id.json().get('name'))
+    with open(filename, "w") as f:
+        r_json = req.json()
+        for i in r_json:
+            if i.get('userId') == int(argv[1]):
+                w = csv.writer(f, quoting=csv.QUOTE_ALL)
+                w.writerow([i.get('userId'), name,
+                           i.get('completed'), i.get('title')])
