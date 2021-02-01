b1ba76ac066c708082ce63e7b019981ef92b6e2e
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-01 17:58:10 +0100

    change logic

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""a Python script that, using this REST API, for a given employee ID,\
- returns information about his/her TODO list progress."""
+""" a Python script that, using this REST API, for a given employee ID,
+ returns information about his/her TODO list progress.
+"""
 
-
+import csv
 import requests
 from sys import argv
-import csv
+
 
 if __name__ == '__main__':
-    if argv[1] is not int:
-        pass
-    id = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' +
-        argv[1])
-    filename = str(id.json()['id']) + ".csv"
 
+    filename = argv[1] + ".csv"
+    id = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
     req = requests.get('https://jsonplaceholder.typicode.com/todos/')
     name = (id.json().get('name'))
-
     with open(filename, "w") as f:
         r_json = req.json()
         for i in r_json:
