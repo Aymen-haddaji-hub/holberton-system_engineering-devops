9c52b28bb0f96760fbe66c7811a55c5c493f87a3
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-02-17 19:21:50 +0100

    pass to check

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+#Using strace, find out why Apache is returning a 500 error.
+#Once you find the issue, fix it and then automate it using Puppet
+#(instead of using Bash as you were previously doing).
+
+exec { 'Fin and replace the wrong syntax':
+    path    => 'usr/bin',
+    command => 'sed -i -e "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
